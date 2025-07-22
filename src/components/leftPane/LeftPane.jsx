1d39dc208e2d4f539d73bf3ecb3687dc4e931697
yasindu2000 <yasindudahanayake@gmail.com>
2025-07-22 11:14:50 +0530

    #1.1 complete left pane🚀

--- src/components/leftPane/LeftPane.jsx
+++ src/components/leftPane/LeftPane.jsx
@@ -11,39 +11,45 @@ import { IoLogOut } from "react-icons/io5";
 
 function LeftPane() {
   return (
-    <div className='flex-[3] className="w-20 md:w-64 bg-white shadow-xl flex flex-col items-center md:items-start p-4 space-y-6'>
-        <h1 className="text-xl font-bold text-center md:text-left">Travelingo</h1>
-           <div className="flex flex-col gap-4 text-gray-500 w-full">
-                <ul  className="flex items-center gap-3 px-3 py-2 bg-green-500 text-white rounded-lg shadow w-full">
+    <div className='flex-[3] w-20 md:w-64 bg-white shadow-xl flex flex-col items-center md:items-start p-10 space-y-6 justify-center gap-10 '>
+        <h1 className="text-3xl font-bold text-center md:text-left ml-8">Travelingo</h1>
+           <div className="flex flex-col gap-5 text-gray-500 w-full items-center justify-center ">
+            
+                <button  className="flex items-center gap-3 px-3 py-2  border-none rounded-2xl cursor-pointer hover:bg-green-500 hover:text-white transition ">
                     <IoHome className="text-lg"/>
                     <span className="hidden md:inline">Dashboard</span>
-                </ul >  
-                <ul  className="flex items-center gap-3 px-3 py-2 w-full">
+                </button >  
+                <button  className="flex items-center gap-3 px-3 py-2 mr-8 border-none rounded-2xl cursor-pointer  hover:bg-green-500 hover:text-white transition ">
                     <BsFillTicketPerforatedFill className="text-lg" />
                     <span className="hidden md:inline">Ticket</span>
-                </ul> 
-                <ul  className="flex items-center gap-3 px-3 py-2 w-full">
+                </button> 
+                <button  className="flex items-center gap-3 px-3 py-2 mr-3 border-none  rounded-2xl cursor-pointer  hover:bg-green-500 hover:text-white transition ">
                     <FaStar  className="text-lg"/>
                     <span className="hidden md:inline">Favourite</span>
-                </ul> 
-                <ul  className="flex items-center gap-3 px-3 py-2 w-full">
+                </button> 
+                <button  className="flex items-center gap-3 px-3 py-2 mr-3  border-none cursor-pointer rounded-2xl  hover:bg-green-500 hover:text-white transition ">
                     <FaMessage className="text-lg"/>
                     <span className="hidden md:inline">Message</span>
-                </ul> 
-                <ul  className="flex items-center gap-3 px-3 py-2 w-full">
+                </button> 
+                <button  className="flex items-center gap-3 px-3 py-2 mr-[-4] border-none cursor-pointer rounded-2xl  hover:bg-green-500 hover:text-white transition ">
                     <GrTransaction className="text-lg"/>
                     <span className="hidden md:inline">Transaction</span>
-                </ul> 
-                <ul  className="flex items-center gap-3 px-3 py-2 w-full">
+                </button> 
+                <button  className="flex items-center gap-3 px-3 py-2 mr-7  border-none cursor-pointer rounded-2xl  hover:bg-green-500 hover:text-white transition ">
                     <RiUserSettingsFill className="text-lg"/>
                     <span className="hidden md:inline">Setting</span>
-                </ul> 
-                <img src={Frame8} alt="Discount" className="w-full hidden md:block" />
+                </button> 
+                
+                <div className="">
+                      <img src={Frame8} alt="Discount" className="w-full hidden md:block ml-[-5] pr-20" />
 
-                 <ul  className="flex items-center gap-3 px-3 py-2 w-full">
+                </div>
+                
+
+                 <button  className="flex items-center gap-3 px-3 py-2 mr-6 border-none cursor-pointer rounded-2xl  hover:bg-green-500 hover:text-white transition ">
                     <IoLogOut className="text-lg"/>
                     <span className="hidden md:inline">Log Out</span>
-                </ul>
+                </button>
                  
            </div>
     
